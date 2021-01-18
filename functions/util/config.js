0bbe5329709ef175198bf003f23c8571a6a0e782
Steve Sultan <59545545+SteveSultan@users.noreply.github.com>
2021-01-18 01:34:48 -0500

    copy config to another file

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //web app's Firebase configuration
+    apiKey: "AIzaSyCgUSz1QuqbDzKcymhGDBq6uhYHQawaaGc",
+    authDomain: "socialapp-6b126.firebaseapp.com",
+    projectId: "socialapp-6b126",
+    storageBucket: "socialapp-6b126.appspot.com",
+    messagingSenderId: "764080498607",
+    appId: "1:764080498607:web:2d3229ad2dba32ee13b15a",
+    measurementId: "G-SFT31XFB2Y"
+};
